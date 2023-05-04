036ec8b5b363408ab2364c61f0f582d382b62b3f
Suraj Singh <112753795+Suraj121704c@users.noreply.github.com>
2023-05-04 10:43:15 +0530

    forecaster

---
+++ forcaster/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Temp from "./Component/Temp";
+
+function App() {
+  return (
+    <div className="App">
+      <Temp />
+    </div>
+  );
+}
+
+export default App;
---
+++ forcaster/src/Component/Temp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./css/style.css";
+
+const Temp = () => {
+  return (
+    <div className="box">
+      <div>
+        <input type="search" className="inputFiled" />
+      </div>
+    </div>
+  );
+};
+
+export default Temp;
